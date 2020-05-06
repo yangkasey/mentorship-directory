311042be825b27f99c42cdd2ff9fde71e4b5fb29
kaseyang <kaseyang@umich.edu>
2020-05-06 16:14:47 -0400

    Added my project

---
+++ js/jquery.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $("#skills").click(function(){
+    console.log("button clicked");
+    $(".form-control").attr("placeholder", "Search for a skill, such as 'accounting'");
+    $(".btn-primary").text("Skill");
+  });
+  $("#location").click(function(){
+    console.log("button clicked");
+    $(".form-control").attr("placeholder", "Search for a location, such as 'Ann Arbor'");
+    $(".btn-primary").text("Location");
+  });
+  $("#mentor-name").click(function(){
+    console.log("button clicked");
+    $(".form-control").attr("placeholder", "Search for a mentor's name, such as 'Sue Dean'");
+    $(".btn-primary").text("Name");
+  });
+});
