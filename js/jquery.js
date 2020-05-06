985a3e2510849abf52d733a98e23e29704ee54a1
kaseyang <kaseyang@umich.edu>
2020-05-06 18:46:35 -0400

    autocomplete function

--- js/jquery.js
+++ js/jquery.js
@@ -15,3 +15,30 @@
     $(".btn-primary").text("Name");
   });
 });
+
+  $( function() {
+    var availableTags = ["accounting", "activism", "advertising", "advocacy", "artificial intelligence", "arts", "athletics", "automotive",
+"autonomous vehicles", "big data", "biodiversity", "bootstrap financing", "budget management",
+"business modeling & planning", "campus knowledge", "child care", "early child development", "chronic illness",
+"climate change", "college access & preparation", "communications", "community organizing",
+"cosmetics", "data anlysis", "data visualization", "dance", "disability issues",
+"design", "economics", "education administration", "education", "higher education", "elder care", "generational divides",
+"emotional intelligence", "engineering", "environmental and ecological protection", "event planning", "facilitation",
+"fashion", "fitness", "food", "food access", "foundations", "fundraising (non-profit)", "fundraising (for-profit)", 
+"gender issues", "general mentorship", "graphic design", "great lakes", "government", "hardware", "health and wellness",
+"healthcare industry", "hiring", "homelessness", "hospitals", "human resources", "humanities", "hunger issues",
+"illustration", "immigration", "infrastructure", "journalism", "language issues", "translation", "law", "life coaching",
+"logistics", "management", "marketing", "maternity", "media", "medicine", "mental health", "mindfulness", "minority communities",
+"music", "natural sciences", "nonprofits", "oceans", "onboarding & training", "operations", "outreach",
+"philanthropy", "photography", "pilot programs", "pitching", "political campaigns",
+"poverty alleviation", "prison issues", "product design", "project management", "prototyping", "publishing",
+"public health", "public relations", "racial justice", "recruitment", "refugees", "relationship building",
+"renewable energy", "research", "rural issues", "self care", "social justice", "social media", "social sciences",
+"software design", "software development", "start-ups", "STEM education",
+"storytelling", "strategic planning", "student organizations", "supply chain management",
+"sustainable business", "transportation", "urban planning", "venture capital", "video production",
+"virtual reality", "waste & recycling", "water issues", "website design", "wildlife issues", "writing & editing"];
+    $( "#ed-srch-term" ).autocomplete({
+      source: availableTags
+    });
+  } );
